8198189fbbbadc1485947f19424a42fe8c9ed112
sus4nier <gr308252@graded.br>
2025-04-24 22:44:25 -0300

    final + errors

--- src/Controller.java
+++ src/Controller.java
@@ -11,6 +11,7 @@ public class Controller {
         } catch (FileNotFoundException e) {
             return;
         }
+        String[] students = mod.getStudents();
         try {
             io.openFile(studentsF);
         } catch (FileNotFoundException e) {
@@ -29,11 +30,40 @@ public class Controller {
                 String nextLine = io.getNextLine();
                 io.output(nextLine);
             }
+             
+            manualGroupChanges(new String[0][0]);
+            return;
         }
         askNumberOfStudents();
+        mod.randomize(students, 12);
+        String[][] groups = splitIntoGroups(students, nOfStudentsInG);
+        for (int i = 0; i < groups.length; i++) {
+            io.output("Group " + (i + 1) + ":");
+            for (int j = 0; j < groups[i].length; j++) {
+                if (groups[i][j] != null) {
+                    io.output("  - " + groups[i][j]);
+                }
+            }
+        }
+
+        String[][] reRandomizedGroups = mod.reRandomizeGroups(students, nOfStudentsInG);
+    if (reRandomizedGroups != null) {
+        groups = reRandomizedGroups; 
+    }
+    manualGroupChanges(groups);
+    io.output("Final groups:");
+    for (int i = 0; i < groups.length; i++) {
+        io.output("Group " + (i + 1) + ":");
+        for (int j = 0; j < groups[i].length; j++) {
+            if (groups[i][j] != null) {
+                io.output("  - " + groups[i][j]);
+            }
+        }
+    }
+    mod.saveToHistory(groups);
     }
 
-    public boolean askImportHistory() {
+    public boolean askImportHistory() { // prompts the user if they want to import a group from history.txt
         while(true) {
         System.out.println("Would you like to import a group from History?");
         String ans = io.input();
@@ -51,11 +81,80 @@ public class Controller {
     }
     }
 
-    public void askNumberOfStudents() {
+    public int nOfStudentsInG = 0;
+
+    public void askNumberOfStudents() { // prompts for number of students in a group (3 or 4 as mentioned by the orginal prompt)
         io.output("How many students per group? Please Write '3' or '4'");
             String number = io.input();
-            int nOfStudentsInG = Integer.valueOf(number); // https://stackoverflow.com/questions/5585779/how-do-i-convert-a-string-to-an-int-in-java
+            nOfStudentsInG = Integer.valueOf(number); // https://stackoverflow.com/questions/5585779/how-do-i-convert-a-string-to-an-int-in-java
     }
+
+    public String[][] splitIntoGroups(String[] students, int groupSize) { // splits the students into groups of 3 or 4
+        int totalStudents = 12;
+        int numGroups = totalStudents / groupSize; 
+        String[][] groups = new String[numGroups][groupSize]; // how to use 2d arrays: https://www.youtube.com/watch?v=alwukGslBG8
+    
+        int sIndex = 0;
+        for (int i = 0; i < numGroups; i++) {
+            for (int j = 0; j < groupSize; j++) {
+                if (sIndex < totalStudents) {
+                    groups[i][j] = students[sIndex++];
+                }
+            }
+        }
+    
+        return groups;
+    }
+
+    public void manualGroupChanges(String[][] groups) { // allows the users to manually change individual students within the groups
+        io.output("Would you like to make manual changes to the groups?");
+        io.output("Input the numbers of the two students to swap in the format 'x, y'. Type 'End' to finish.");
+
+        while (true) {
+            String input = io.input();
+
+            if (input.equalsIgnoreCase("End")) {
+                break; 
+            }
+
+            try {
+                // split the input into two student numbers
+                String[] indices = input.split(",");
+                int x = Integer.parseInt(indices[0].trim());
+                int y = Integer.parseInt(indices[1].trim());
+
+                // finds the positions of the students in the groups
+                int[] posX = findStudentPosition(groups, x);
+                int[] posY = findStudentPosition(groups, y);
+
+                if (posX != null && posY != null) {
+                    // swap the students in the groups
+                    String temp = groups[posX[0]][posX[1]];
+                    groups[posX[0]][posX[1]] = groups[posY[0]][posY[1]];
+                    groups[posY[0]][posY[1]] = temp;
+
+                    io.output("Swapped student " + x + " with student " + y);
+                } else {
+                    io.output("Invalid student numbers. Please try again.");
+                }
+            } catch (Exception e) {
+                io.output("Invalid input format. Please use 'x, y' or type 'End' to finish.");
+            }
+        }
+    }
+
+    private int[] findStudentPosition(String[][] groups, int studentNumber) {
+    int count = 1;
+    for (int i = 0; i < groups.length; i++) {
+        for (int j = 0; j < groups[i].length; j++) {
+            if (groups[i][j] != null && count == studentNumber) {
+                return new int[]{i, j}; 
+            }
+            count++;
+        }
+    }
+    return null;
+}
 
 
 
--- src/Model.java
+++ src/Model.java
@@ -2,25 +2,29 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Random;
 import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Model {
     private View io = new View();
+    private Controller con = new Controller();
     private Scanner fileInput;
     private File file;
+    private String[] students = new String[12];
 
-     public void fileToArr(String filename) throws FileNotFoundException { // converts the students into a array https://stackoverflow.com/questions/22059593/read-file-then-store-in-array-without-using-arraylist-java
-        File file = new File(filename);
-        int linecount = 12;
-        String[] students = new String[12];
-
-        fileInput = new Scanner(file);
-        while(io.fileHasNextLine()) {
-            for (int i = 0; i < 12; i++) {
-                students[i] = io.getNextLine();
-            }
+    public void fileToArr(String filename) throws FileNotFoundException { // converts the students.txt file into a array of strings
+        io.openFile(filename); 
+        students = new String[12];
+    
+        int index = 0;
+        while (io.fileHasNextLine() && index < students.length) {
+            students[index++] = io.getNextLine(); 
         }
+    
+    }
 
-
+    public String[] getStudents() { // returns the students array to the controller
+        return students;
     }
     static void randomize(String students[], int n) { // shuffles the basic student list using fisher-yates algorithm (idea by Mr Griswold) concept converted to java code in https://www.geeksforgeeks.org/shuffle-a-given-array-using-fisher-yates-shuffle-algorithm/
         Random r = new Random();
@@ -31,5 +35,58 @@ public class Model {
             students[rIndex] = tempAr;
         }
     }
+    public String[][] reRandomizeGroups(String[] students, int groupSize) { // re-randomizes the groups if the user wants to using the same randomization and output process as the first time
+        String[][] groups = null;
+    
+        while (true) {
+            io.output("Would you like to re-randomize the groups? (Yes/No)");
+            String response = io.input();
+    
+            if (response.equalsIgnoreCase("Yes")) {
+                randomize(students, students.length);
+
+                groups = con.splitIntoGroups(students, groupSize);
+    
+                io.output("Re-randomized groups:");
+                for (int i = 0; i < groups.length; i++) {
+                    io.output("Group " + (i + 1) + ":");
+                    for (int j = 0; j < groups[i].length; j++) {
+                        if (groups[i][j] != null) {
+                            io.output("  - " + groups[i][j]);
+                        }
+                    }
+                }
+            } else if (response.equalsIgnoreCase("No")) {
+                io.output("Re-randomization process ended.");
+                break;
+            } else {
+                io.output("Invalid input. Please enter 'Yes' or 'No'.");
+            }
+        }
+    
+        return groups; // returns the final groups to the controller
+    }
+    public void saveToHistory(String[][] groups) { // saves the groups to a history file if the user wants to // FileWriter was learned @ https://www.youtube.com/watch?v=kjzmaJPoaNc
+        io.output("Would you like to save the current groups to history? (Yes/No)");
+        String response = io.input();
+    
+        if (response.equalsIgnoreCase("Yes")) {
+            try (FileWriter writer = new FileWriter("history.txt", false)) { 
+                for (int i = 0; i < groups.length; i++) {
+                    writer.write("Group " + (i + 1) + ":\n");
+                    for (int j = 0; j < groups[i].length; j++) {
+                        if (groups[i][j] != null) {
+                            writer.write("  - " + groups[i][j] + "\n");
+                        }
+                    }
+                }
+                io.output("Groups successfully saved to history.");
+            } catch (IOException e) {
+            }
+        } else {
+            io.output("Groups were not saved to history.");
+        }
+    }
+    
 
 }
